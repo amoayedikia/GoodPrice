68f927e9d58e8e3f8bdbcf88b5375a034a93418b
Lucifer313 <39132464+Lucifer313@users.noreply.github.com>
2021-09-04 18:03:55 +1000

    Redux setup done and Responseive UI Changes

--- goodprice-front/src/components/ProductPanel/components/ProductItems/components/ProductItem/ProductItem.js
+++ goodprice-front/src/components/ProductPanel/components/ProductItems/components/ProductItem/ProductItem.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { Col, Card, Button } from "react-bootstrap";
 import "./ProductItem.scss";
@@ -22,6 +23,32 @@ const ProductItem = () => (
       </Card.Body>
     </Card>
   </Col>
+=======
+import React from 'react';
+import { 
+    Col,
+    Card,
+    Button
+} from 'react-bootstrap';
+import './ProductItem.scss';
+
+const ProductItem = () => (
+    <Col lg={4} md={12}>
+    <Card className="product__card m-2" border-color="primary">
+        <Card.Img variant="top" src="assets/images/bicycles.png" className="product__image" />
+        <Card.Body>
+            <Card.Title>Product Name</Card.Title>
+            <Card.Text>
+                Product Description
+            </Card.Text>
+            <hr />
+            <div>Price</div>
+            <div>Supplier Name</div>
+            <Button variant="primary" className="product__button">View Product</Button>
+        </Card.Body>
+    </Card>
+    </Col>
+>>>>>>> 9dc94ec (Redux setup done and Responseive UI Changes)
 );
 
 export default ProductItem;
