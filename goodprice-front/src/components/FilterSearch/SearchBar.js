2b60d281b0bcdff71f61367919581f2b84d55656
Shikai Wang <skykk0128@gmail.com>
2021-09-09 00:08:13 +1000

    GP-02: Fix layout and format code

--- goodprice-front/src/components/FilterSearch/SearchBar.js
+++ goodprice-front/src/components/FilterSearch/SearchBar.js
@@ -1,29 +1,22 @@
-import React from 'react';
-import {
-    Nav,
-    Form,
-    FormControl,
-    Button
-} from 'react-bootstrap';
-import Dropdowns from './components/Dropdowns/Dropdowns';
-import './SearchBar.scss';
+import React from "react";
+import { Nav, Form, FormControl, Button } from "react-bootstrap";
+import Dropdowns from "./components/Dropdowns/Dropdowns";
+import "./SearchBar.scss";
 
 const SearchBar = () => (
-    <Nav variant="pills" className="search">
-        <div className="currentlocation">
-            Your location: Hawthorn VIC 3122
-        </div>
-        <Dropdowns />
-        <Form className="d-flex search__form">
-            <FormControl
-                type="search"
-                placeholder="Search products"
-                className="mr-4"
-                aria-label="Search"
-            />
-            <Button variant="outline-dark">Search</Button>
-        </Form>
-    </Nav>
+  <Nav variant="pills" className="search">
+    <div className="currentlocation">Your location: Hawthorn VIC 3122</div>
+    <Dropdowns />
+    <Form className="d-flex search__form">
+      <FormControl
+        type="search"
+        placeholder="Search products"
+        className="mr-4"
+        aria-label="Search"
+      />
+      <Button variant="outline-dark">Search</Button>
+    </Form>
+  </Nav>
 );
 
 export default SearchBar;
--- goodprice-front/src/components/ProductPanel/ProductPanel.js
+++ goodprice-front/src/components/ProductPanel/ProductPanel.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { 
-    Container
-} from 'react-bootstrap';
-import ProductItems from './components/ProductItems/ProductItems';
-import './ProductPanel.scss';
+import React from "react";
+import { Container } from "react-bootstrap";
+import ProductItems from "./components/ProductItems/ProductItems";
+import "./ProductPanel.scss";
 
 const ProductPanel = () => (
-    <Container className="product-panel">
-            <ProductItems />
-    </Container>
+  <Container className="product-panel">
+    <ProductItems />
+  </Container>
 );
 
 export default ProductPanel;
