93c909a6a7adc41732cc243db3003762f000319b
Lucifer313 <39132464+Lucifer313@users.noreply.github.com>
2021-10-02 21:58:37 +1000

    Profile Login and Register screens added

--- goodprice-front/src/components/Navbar/NavigationBar.js
+++ goodprice-front/src/components/Navbar/NavigationBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Navbar,
   Container,
@@ -6,35 +6,35 @@ import {
   Form,
   FormControl,
   Button,
-} from "react-bootstrap";
-import UserAvatar from "./components/UserAvatar/UserAvatar";
-import './NavigationBar.scss';
+} from 'react-bootstrap'
+import UserAvatar from './components/UserAvatar/UserAvatar'
+import './NavigationBar.scss'
 
 const NavigationBar = () => (
-  <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
-    <Container className="navigation">
-      <Navbar.Brand href="">Good Price</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="">Home</Nav.Link>
-          <Nav.Link href="">About Us</Nav.Link>
-          <Nav.Link href="">FAQ</Nav.Link>
+  <Navbar collapseOnSelect expand='lg' bg='light' variant='light' sticky='top'>
+    <Container className='navigation'>
+      <Navbar.Brand href=''>Good Price</Navbar.Brand>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+      <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav className='me-auto'>
+          <Nav.Link href=''>Home</Nav.Link>
+          <Nav.Link href=''>About Us</Nav.Link>
+          <Nav.Link href=''>FAQ</Nav.Link>
         </Nav>
-        <Form className="d-flex">
+        <Form className='d-flex'>
           <FormControl
-            type="search"
-            placeholder="Search"
-            className="mr-4"
-            aria-label="Search"
+            type='search'
+            placeholder='Search'
+            className='mr-4'
+            aria-label='Search'
           />
-          <Button variant="outline-dark">Search</Button>
+          <Button variant='outline-dark'>Search</Button>
         </Form>
-        <div className="space"></div>
+        <div className='space'></div>
         <UserAvatar />
       </Navbar.Collapse>
     </Container>
   </Navbar>
-);
+)
 
-export default NavigationBar;
+export default NavigationBar
